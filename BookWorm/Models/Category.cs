39059f2751b959a9279294832993f266aec84319
Chelsie Manalastas <manalastaschelsie@gmail.com>
2024-07-28 17:03:08 +0800

    Defined connection string and declared AppDbContext for database connection

---
+++ BookWorm/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookWorm.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
