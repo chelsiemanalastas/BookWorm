647c55c47ec6026da82c9719628eed9de9e70628
Chelsie Manalastas <manalastaschelsie@gmail.com>
2024-08-03 22:21:48 +0800

    Created and implemented Category interface

---
+++ Book.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using Book.DataAccess.Repository.IRepository;
+using Book.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private AppDbContext _db;
+
+        public CategoryRepository(AppDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _db.Categories.Update(category);
+        }
+    }
+}
