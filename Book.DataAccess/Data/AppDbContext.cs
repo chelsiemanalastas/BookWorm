5d63e39680e96ec974fc75e1e98944a9eae65b3a
Chelsie Manalastas <manalastaschelsie@gmail.com>
2024-08-31 22:52:13 +0800

    Extend Identity User database schema model -add migration -update database

--- Book.DataAccess/Data/AppDbContext.cs
+++ Book.DataAccess/Data/AppDbContext.cs
@@ -14,6 +14,7 @@ namespace Book.DataAccess
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
