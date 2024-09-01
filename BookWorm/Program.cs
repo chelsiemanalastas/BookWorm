7a5e7c604d1d52028596dd971b8f9ff6cbc795b2
Chelsie Manalastas <manalastaschelsie@gmail.com>
2024-09-01 13:32:07 +0800

    Assign user roles on Register Account -Allow selection of user role upon account registration -Make default user role to Customer when no role selected

--- BookWorm/Program.cs
+++ BookWorm/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddDbContext<AppDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
 );
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
